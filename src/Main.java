5a5a03190ba5beb3b1824a63ad1c0ed04bfa3686
Kirill <nosolution01@gmail.com>
2018-10-14 01:19:12 +0400

    Added 6th task implemented using HashSet

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,6 @@ public class Main {
     public static void main(String[] args) {
     StringHandler stringHandler = new StringHandler();
     stringHandler.doLongestShortestStringLength();
+    stringHandler.doWordsLengthComparison();
     }
 }
--- src/StringHandler.java
+++ src/StringHandler.java
@@ -5,14 +5,14 @@
  * Author: Kirill Kuzin
  * Lesson 1 Task 2
  * */
-import java.util.Scanner;
+import java.util.*;
 
 public class StringHandler {
     public void doLongestShortestStringLength(){
         Scanner in = new Scanner(System.in);
         int longestStringIndex = 0;
         int shortestStringIndex = 0;
-        System.out.println("Enter amount of strings:");
+        System.out.println("First task. \nEnter amount of strings:");
         if(in.hasNextInt()){
             int n = Integer.parseInt(in.nextLine());  //parseInt() added because of nextInt() ignoring \n symbol (so nextLine() gonna return empty string)
             String[] targetStringArray= new String[n];
@@ -28,7 +28,26 @@ public class StringHandler {
             }
             System.out.println("Shortest string: \"" + targetStringArray[shortestStringIndex] + "\"; length: " + targetStringArray[shortestStringIndex].length());
             System.out.println("Longest string: \"" + targetStringArray[longestStringIndex] + "\"; length: " + targetStringArray[longestStringIndex].length());
-
         }
     }
+    public void doWordsLengthComparison(){
+        String[] words;
+        String text;
+        int minAmountOfDiffSymbols = Integer.MAX_VALUE;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Forth task. \nEnter words divided with 'space':");
+        text = in.nextLine();
+        words = text.split(" ");
+        String wordWithLesserUniqChars = words[0];
+        int amountOfUniqChars = new HashSet<String> (Arrays.asList(words[0].split(""))).size();
+        for (String word : words){
+            Set<String> hashSet = new HashSet<String> (Arrays.asList(word.split("")));
+            if (hashSet.size() < amountOfUniqChars){
+                wordWithLesserUniqChars = word;
+                amountOfUniqChars = hashSet.size();
+            }
+        }
+        System.out.println("Word with lesser amount of unique chars is \"" + wordWithLesserUniqChars  +"\".");
+    }
 }
+
